b823e17fb8ea0383282da4c53466404a6d3c5c33
Akihiko Nakamura <anakamuro@earthlink.net>
2024-05-08 19:42:27 -0400

    commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,30 +1,24 @@
-import React, {useContext} from 'react';
-import {products} from "./products.js"
+import React from 'react';
 import '../App.css';
-import {ShopContext} from "../context/ShopContext.js"
 
-function Home() {
-    const {addToCart, cartItems} = useContext(ShopContext);
+function Home({addToCart, removeToCart, products}) {
   return (
     <div className='container'>
-        {
-         products.map((product) => {
-            const {id, image, name, price}= product;
-            const cartItemAmount = cartItems[id];
-            return (
-                <div className="cards" key={id}>
+        {products.map((product) => (
+            // const {id, image, name, price}= product;
+                <div className="cards" key={product.id}>
                     <div className="image">
-                        <img className="image" src={image} alt={name}></img>
+                        <img className="image" src={product.image} alt={product.name}></img>
                     </div>
                     <div className="details">
-                        <h4>{name}</h4>
-                        <p>${price}</p>
+                        <h4>{product.name}</h4>
+                        <p>${product.price}</p>
+                        <p>{product.length}</p>
                     </div>
-                    <button className="btn" onClick={() => addToCart(id)}>Add To Cart {cartItemAmount > 0 && <> ({cartItemAmount})</>}</button>
+                    <button className="btn" onClick={() => addToCart(product)}>Add To Cart</button>
+                    <button className="btn" onClick={() => removeToCart(product)}>Remove To Cart</button>
                 </div>
-            )
-        })
-        }
+         ))}
     </div>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,9 @@ import React from 'react';
 import {NavLink} from "react-router-dom";
 import "./Navbar.css";
 
-function Navbar() {
+
+function Navbar(props) {
+  const {countCartItems} = props;
   return (
     <nav>
         <div>
@@ -11,7 +13,9 @@ function Navbar() {
                 <NavLink to="/">   <img src="https://th.bing.com/th?id=OIP.tu5yvoyO5oYFWYzLVOf-BwHaHa&w=250&h=250&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" alt="icon"/></NavLink>
             </li>
             <li>
-                <NavLink to="/cart"><i className="fa-solid fa-cart-shopping"></i></NavLink>
+              {countCartItems > 0 &&
+                <NavLink to="/cart"><i className="fa-solid fa-cart-shopping"></i>{countCartItems}</NavLink>
+               }
             </li>
         </ul>
         </div>
